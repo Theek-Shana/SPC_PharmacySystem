79e8fa895f3f4818773a5062ca78274afe22330f
Theekshana Vidushan <mahesh190290390@gmail.com>
2025-12-26 22:03:44 +0530

    Update DTOSPCSupplierWrite.cs

--- DTOSPCSupplierWrite.cs
+++ DTOSPCSupplierWrite.cs
@@ -6,7 +6,8 @@
         public string SupplierName { get; set; }
         public string SupplierAddress { get; set; }
         public string SupplierPhoneNumber { get; set; }
-        public string SupplierEmail { get; set; }
+        public string SupplierEmail { get; set; } 
 
     }
 }
+
